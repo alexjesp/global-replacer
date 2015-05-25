ea7ed3216c884c3ebc5a55b82e88cff0de0b9717
alexjesp <alexjesp@gmail.com>
2015-05-25 20:44:32 +0100

    Add more tests and move example folder

--- test/replacer-test.js
+++ test/replacer-test.js
@@ -49,14 +49,61 @@ describe('Global replacer', function () {
   });
 
   it('should replace multiple unique globals on the same line', function () {
-
+    var s = replacer('window.location.href = document.location.href.replace(/www\\./, "subdomain");', {
+      replacements: {
+        'window': '_window',
+        'document': '__document'
+      }
+    });
+    expect(s).to.be.eql('_window.location.href = __document.location.href.replace(/www\\./, "subdomain");');
   });
 
   it('should handle multiple global replacements', function () {
-
+    var s = replacer('window.location.href;\nlocation;', {
+      replacements: {
+        'window': '_window',
+        'location': '_l_ocation'
+      }
+    });
+    expect(s).to.be.eql('_window.location.href;\n_l_ocation;');
   });
 
   it('should handle multiple global and property replacements', function () {
+    var s = replacer('' +
+      'window.addEventListener();\n' +
+      'location.href = "http://google.com";\n' +
+      'document.location.assign(window.location.href.toString() ? "/blah" : "/hello");', {
+      replacements: {
+        'window': '_window',
+        'location': '_l_ocation',
+        'document.location': 'document._l_ocation'
+      }
+    });
+    expect(s).to.be.eql('' +
+      '_window.addEventListener();\n' +
+      '_l_ocation.href = "http://google.com";\n' +
+      'document._l_ocation.assign(_window.location.href.toString() ? "/blah" : "/hello");'
+    );
+  });
+
+  it('should not matter which order the replacements are passed', function () {
+    var s1 = replacer('window.location.href;\nlocation;', {
+      replacements: {
+        'window': '_window',
+        'location': '_l_ocation'
+      }
+    });
+    var s2 = replacer('window.location.href;\nlocation;', {
+      replacements: {
+        'location': '_l_ocation',
+        'window': '_window'
+      }
+    });
+    expect(s1).to.be.eql('_window.location.href;\n_l_ocation;');
+    expect(s2).to.be.eql('_window.location.href;\n_l_ocation;');
+  });
+
+  it.skip('should handle global and it\'s property on the same line', function () {
 
   });
 
